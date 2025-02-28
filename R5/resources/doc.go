235d752e5bd3d0d162decd23eec392729c68e8ab
Dmitry Kalinin <kalinin.mitko@gmail.com>
2025-03-01 01:53:59 +0200

    Added R5 #1

---
+++ R5/resources/doc.go
@@ -0,0 +1,10 @@
+// Package resources provides all resources implementation of FHIR v5.0.0
+//
+// Reference: https://www.hl7.org/fhir/resourcelist.html
+/*
+Identification Individuals:
+	Patient
+	Practitioner
+	RelatedPerson
+*/
+package resources
